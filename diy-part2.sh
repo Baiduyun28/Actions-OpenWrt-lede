4e2fc8aeddc461f22dec6771a33eb2b7b75dbbaf
Baiduyun28 <97877618+Baiduyun28@users.noreply.github.com>
2023-09-26 09:13:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,14 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 修改默认IP为 10.0.0.2
+#sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
+ 
+# 修改默认主机名
+#sed -i '/uci commit system/iuci set system.@system[0].hostname='Soft_Router'' package/lean/default-settings/files/zzz-default-settings
+ 
+# 加入编译者信息
+#sed -i "s/OpenWrt /Kinoko build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+ 
+# 修改默认主题
+#sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
