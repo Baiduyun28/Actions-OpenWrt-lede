91cf4d55a612bd8bc83fd6e79b07fe85f717e342
Baiduyun28 <97877618+Baiduyun28@users.noreply.github.com>
2023-09-26 09:58:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,14 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 添加 OpenAppFilter 应用过滤插件
+
+#git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
+#rm -rf package/lean/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# 替换默认主题为 luci-theme-argon
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
+#git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
