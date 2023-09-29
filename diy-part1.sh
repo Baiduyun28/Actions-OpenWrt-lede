e64bd3bffc783a52af3b490cdf2f7a998a22739c
Baiduyun28 <97877618+Baiduyun28@users.noreply.github.com>
2023-09-29 15:23:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
@@ -25,6 +16,7 @@ rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
 
+# 切换传统交换机swconfig
 sed -i '132,153d' ./target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
 sed -i '131a &ethernet {\n	compatible = "mediatek,ralink-mt7621-eth";\n	mediatek,switch = <&gsw>;\n};\n\n&gsw {\n	compatible = "mediatek,ralink-mt7621-gsw";' ./target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
 sed -i '64a 	hiwifi,hc5962)\n		ucidef_add_switch "switch0" \\n			"1:lan" "2:lan" "3:lan" "4:wan" "6@eth0"\n		;;' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
