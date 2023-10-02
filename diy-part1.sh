9ac0e3013e71dc5fdeec94aa93f1e2868e9d8c6d
Baiduyun28 <97877618+Baiduyun28@users.noreply.github.com>
2023-10-02 22:05:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,20 +8,21 @@ echo 'src-git luci-app-autotimeset  https://github.com/sirpdboy/luci-app-autotim
 echo 'src-git luci-app-ddns-go  https://github.com/sirpdboy/luci-app-ddns-go' >>feeds.conf.default
 echo 'src-git luci-app-eqosplus  https://github.com/sirpdboy/luci-app-eqosplus' >>feeds.conf.default
 echo 'src-git luci-app-parentcontrol  https://github.com/sirpdboy/luci-app-parentcontrol' >>feeds.conf.default
-svn export https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-cpulimit package/luci-app-cpulimit
-svn export https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-control-timewol package/luci-app-control-timewol
+svn export https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-cpulimit package/luci-app-cpulimit #版本更新
+#svn export https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-control-timewol package/luci-app-control-timewol #2.2-20200927
 svn export https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-watchcat-plus package/luci-app-watchcat-plus
 
-echo 'src-git go-aliyundrive-webdav  https://github.com/jerrykuku/luci-app-go-aliyundrive-webdav' >>feeds.conf.default
+echo 'src-git luci-app-go-aliyundrive-webdav  https://github.com/jerrykuku/luci-app-go-aliyundrive-webdav' >>feeds.conf.default
+echo 'src-git go-aliyundrive-webdav  https://github.com/jerrykuku/go-aliyundrive-webdav' >>feeds.conf.default
 
-svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-control-timewol package/luci-app-control-timewol
+svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-control-timewol package/luci-app-control-timewol #1.0-3-20220406
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-control-webrestriction package/luci-app-control-webrestriction
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-control-weburl package/luci-app-control-weburl
-svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-timecontrol package/luci-app-timecontrol
+svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-timecontrol package/luci-app-timecontrol #1.1-20220505
 
-svn export https://github.com/immortalwrt/luci/trunk/openwrt-18.06/applications/luci-app-cpulimit package/luci-app-cpulimit
-svn export https://github.com/immortalwrt/luci/trunk/openwrt-18.06/applications/luci-app-timecontrol package/luci-app-timecontrol
-svn export https://github.com/immortalwrt/luci/trunk/openwrt-18.06/applications/luci-app-timewol package/luci-app-timewol
+#svn export https://github.com/immortalwrt/luci/trunk/openwrt-18.06/applications/luci-app-cpulimit package/luci-app-cpulimit #版本旧
+#svn export https://github.com/immortalwrt/luci/trunk/openwrt-18.06/applications/luci-app-timecontrol package/luci-app-timecontrol #1.0-1-20200402
+#svn export https://github.com/immortalwrt/luci/trunk/openwrt-18.06/applications/luci-app-timewol package/luci-app-timewol #1.0-3-20190309
 
 #git_clone https://github.com/messense/aliyundrive-webdav aliyundrive && mv -n aliyundrive/openwrt/* ./ ; rm -rf aliyundrive
 #git_clone https://github.com/messense/aliyundrive-fuse aliyundrive && mv -n aliyundrive/openwrt/* ./;rm -rf aliyundrive
