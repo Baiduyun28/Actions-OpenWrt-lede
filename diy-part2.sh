dfb38c27cee64c320a1826638bc3bd16f5c3c0a3
Baiduyun28 <97877618+Baiduyun28@users.noreply.github.com>
2023-10-07 23:22:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,6 +18,14 @@
  
 # 加入编译者信息
 #sed -i 's/OpenWrt /Kinoko build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g' package/lean/default-settings/files/zzz-default-settings
- 
+
 # 修改默认主题
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/collections/luci/Makefile
+
+# 切换传统交换机swconfig
+sed -i '132,153d' ./target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
+sed -i '131a &ethernet {\n	compatible = "mediatek,ralink-mt7621-eth";\n	mediatek,switch = <&gsw>;\n};\n\n&gsw {\n	compatible = "mediatek,ralink-mt7621-gsw";' ./target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
+sed -i '64a 	hiwifi,hc5962)\n		ucidef_add_switch "switch0" \\n			"1:lan" "2:lan" "3:lan" "4:wan" "6@eth0"\n		;;' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
+sed -i '25d' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
