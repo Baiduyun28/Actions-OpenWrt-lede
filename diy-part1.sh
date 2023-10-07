4365e5c57b4f5c90050c48db178876e64da81bab
Baiduyun28 <97877618+Baiduyun28@users.noreply.github.com>
2023-10-07 23:21:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -38,13 +38,3 @@ svn export https://github.com/sirpdboy/sirpdboy-package/trunk/cpulimit package/c
 #git_clone https://github.com/zzsj0928/luci-app-pushbot
 
 cat feeds.conf.default
-
-# 修改默认主题
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
-# 切换传统交换机swconfig
-sed -i '132,153d' ./target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
-sed -i '131a &ethernet {\n	compatible = "mediatek,ralink-mt7621-eth";\n	mediatek,switch = <&gsw>;\n};\n\n&gsw {\n	compatible = "mediatek,ralink-mt7621-gsw";' ./target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
-sed -i '64a 	hiwifi,hc5962)\n		ucidef_add_switch "switch0" \\n			"1:lan" "2:lan" "3:lan" "4:wan" "6@eth0"\n		;;' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-sed -i '25d' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
